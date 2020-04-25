5d22a700ae7c3640ab87b280e531b16d12d05104
Smit Shrestha <smitshrestha101@gmail.com>
2020-04-25 15:11:50 -0500

    added foldingcell library

---
+++ app/src/main/java/com/chiragawale/hungrypeople/Home/ui/home/HomeFragment.java
@@ -0,0 +1,96 @@
+package com.chiragawale.hungrypeople.Home.ui.home;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import com.chiragawale.hungrypeople.Home.Item;
+import com.chiragawale.hungrypeople.R;
+import com.ramotion.foldingcell.FoldingCell;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel mViewModel;
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.home_fragment, container, false);
+
+        ListView listView=v.findViewById(R.id.mainListView);
+
+        final List<Item> items = Item.getTestingList();
+
+        // add custom btn handler to first list item
+        items.get(0).setRequestBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "CUSTOM HANDLER FOR FIRST BUTTON", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        final FoldingCellListAdapter adapter = new FoldingCellListAdapter(getActivity(),items);
+
+        // add default btn handler for each request btn on each item if custom handler not found
+        adapter.setDefaultRequestBtnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "DEFAULT HANDLER FOR ALL BUTTONS", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // set elements to adapter
+        listView.setAdapter(adapter);
+
+        // set on click event listener to list view
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                // toggle clicked cell state
+                Log.e("clicked","clicked");
+                ((FoldingCell) view).toggle(false);
+                // register in adapter that state for selected cell is toggled
+                adapter.registerToggle(pos);
+            }
+        });
+
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
